50c9873c57fe284378d8ddad1af95aa8e6e7b9bb
antonio <antoniocorderomn@gmail.com>
2026-02-07 13:56:00 +0100

    Falta hacer el spawn correcto

--- Escenas Estaticas/staticScene2.gd
+++ Escenas Estaticas/staticScene2.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
-@onready var dialogue_resource = preload("res://Dialogues/initial.dialogue")
+@onready var dialogue_resource = preload("res://Dialogues/dialogo_sala2.dialogue")
 
 func _ready():
+	print("staticScene2 LISTA!")
+	
 	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 	await get_tree().create_timer(1.0).timeout
+	print("Mostrando diálogo:", dialogue_resource)  # ← DEBUG
 	DialogueManager.show_dialogue_balloon(dialogue_resource, "start")
 
 func _on_dialogue_ended(_resource):
-	GameManager.show_static_scene(1)  # ID 1 para escenas_estaticas.tscn
+	print("Final → Level1")
+	GameManager.start_level(1)
