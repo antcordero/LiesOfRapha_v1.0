e881076f698b099ebcfcd161387439bfcce83779
Jose Antonio <josevillodres94@gmail.com>
2026-02-21 11:30:25 +0100

    Sistema de inventario

---
+++ items/Item.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name Item
+
+@export var name: String
+@export var icon: Texture2D
+@export var stackable: bool = true
+@export var sum: int = 1
+@export var max_sum: int = 99999
