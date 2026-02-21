80d151eaacae44a9f8d09874dddc4fbddf2f57b4
antonio <antoniocorderomn@gmail.com>
2026-02-21 11:40:34 +0100

    Actualizazión

--- LiesOfRapha_v1.0/Scenes/Level_2_scene/level_end_area_2.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node2D) -> void:
-	print("BODY ENTERED LEVEL 2:", body)
-	print("LLAMANDO A start_level(3)")
-	GameManager.start_level(3)
--- LiesOfRapha_v1.0/items/Item.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name Item
-
-@export var name: String
-@export var icon: Texture2D
-@export var stackable: bool = true
-@export var sum: int = 1
-@export var max_sum: int = 99999
